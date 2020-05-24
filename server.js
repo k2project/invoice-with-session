8fa1dd9d2304bc9619c75cee32bec4d603062d4e
k2projct <kopczkrzy@gmail.com>
2020-05-24 13:14:25 +0100

    deploying to heroku

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ const IN_PROD = NODE_ENV === 'production';
         app.use('/api/profile', profileRoutes);
         app.use('/api/companies', companiesRoutes);
         if (process.env.NODE_ENV === 'production') {
-            app.use(express.static('client/build'));
+            app.use(express.static(path.join(__dirname, 'client/build')));
             app.get('*', (req, res) => {
                 res.sendFile(
                     path.resolve(__dirname, 'client', 'build', 'index.html')
